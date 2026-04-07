58d164019a983897382d5d0e20b555b0979510af
Malik8122 <sanyask.malik@gmail.com>
2026-04-07 20:51:29 +0530

    live demo update

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 # Page Configuration
 # -------------------------------
 st.set_page_config(
-    page_title="HistoScan AI 🚀",
+    page_title="HistoScan AI",
     page_icon="🧬",
     layout="wide"
 )
